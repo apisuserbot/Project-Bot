c68a380cc303fc23ead33c65cc59b5c01357d680
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-30 06:43:12 +0700

    fixes

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-TOKEN = "1626343681:AAFBJpoaRtZHmAU7A4Wj2VFChGinno3IpWI"
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+ADMIN = os.environ.get("ADMIN", "")
+OWNER = os.environ.get("OWNER", "")
+GROUP = os.environ.get("GROUP", "cari_teman_online_group")
+CHANNEL = os.environ.get("CHANNEL", "quotes_channel_official")
+PROJECT_NAME = os.environ.get("PROJECT_NAME", "PROJECT BOT")
