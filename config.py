33923f9738fd94c07dd3b17a9b85660e62800f07
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-30 07:06:19 +0700

    fixes

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 TOKEN = os.environ.get("TOKEN", "")
 ADMIN = os.environ.get("ADMIN", "")
 OWNER = os.environ.get("OWNER", "")
+INSTAGRAM = os.environ.get("INSTAGRAM", "")
 GROUP = os.environ.get("GROUP", "cari_teman_online_group")
 CHANNEL = os.environ.get("CHANNEL", "quotes_channel_official")
 PROJECT_NAME = os.environ.get("PROJECT_NAME", "PROJECT BOT")
